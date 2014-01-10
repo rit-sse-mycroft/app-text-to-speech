2225326f3026570ac64b1874662776bc390b86c8
Robert McLaughlin <robert@sparkk.us>
2014-01-10 11:40:53 -0500

    create simple 'hello world' sample

--- AppTextToSpeech/Program.cs
+++ AppTextToSpeech/Program.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
-namespace AppTextToSpeech
+namespace SimpleSpeak
 {
   class Program
   {
     static void Main(string[] args)
     {
+      SpeechSynthesizer s = new SpeechSynthesizer();
+      s.Speak("Hello. My name is Microsoft Anna.");
+
     }
   }
 }
