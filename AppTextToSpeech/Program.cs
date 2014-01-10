8a733ea92907f12a613f416fcfb83bc4f28a5642
Ethan H. Jurman <ehj2229@rit.edu>
2014-01-10 13:56:58 -0500

    updated SimpleSpeak.cs

--- AppTextToSpeech/Program.cs
+++ AppTextToSpeech/Program.cs
@@ -6,13 +6,41 @@ using System.Speech.Synthesis;
 
 namespace SimpleSpeak
 {
+  enum AudioSource { AudioStream, DefaultAudioDevice, Null, WaveFile, WaveStream };
+    
   class Program
   {
+    static void Speech(string speechText, AudioSource outputSelection)
+    {
+      SpeechSynthesizer synth = new SpeechSynthesizer();
+      AudioSource source = outputSelection;
+      switch (source)
+      {
+        //case AudioSource.AudioStream:
+        //  synth.SetOutputToAudioStream();
+        //  break;
+        case AudioSource.Null:
+          synth.SetOutputToNull();
+          break;
+        //case AudioSource.WaveFile:
+        //  synth.SetOutputToWaveFile();
+        //  break;
+        //case AudioSource.WaveStream:
+        //  synth.SetOutputToWaveStream();
+        //  break;
+        case AudioSource.DefaultAudioDevice:
+        default:
+          synth.SetOutputToDefaultAudioDevice();
+          break;
+      }
+      synth.Speak(speechText);
+    } 
     static void Main(string[] args)
     {
-      SpeechSynthesizer s = new SpeechSynthesizer();
-      s.Speak("Hello. My name is Microsoft Anna.");
-
+      string speechText = "";
+      AudioSource audioSource = AudioSource.DefaultAudioDevice;
+      //TODO: MycroftClient defines text and source
+      Speech(speechText, audioSource);
     }
   }
 }
