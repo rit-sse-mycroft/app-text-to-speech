b9489a301ac2ec7cd087b541adf3d1f5b4291ecc
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2014-01-13 14:02:51 -0500

    Mycroft has a voice

---
+++ AppTextToSpeech/MycroftVoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace AppTextToSpeech
+{
+    class MycroftVoice
+    {
+        private SpeechSynthesizer synth;
+        public MycroftVoice(){
+            InitializeVoice();
+        }
+
+        private void InitializeVoice()
+        {
+            synth = new SpeechSynthesizer();
+            synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0,
+                new System.Globalization.CultureInfo("en-GB"));
+        }
+
+        public void SayMessage(string message)
+        {
+            synth.Speak(message);
+        }
+    }
+}
