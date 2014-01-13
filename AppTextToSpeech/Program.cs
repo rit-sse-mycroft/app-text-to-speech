ad57cbf819c3d8604c06a3037d66d7baf7f55549
Robert McLaughlin <robert@sparkk.us>
2014-01-13 13:15:05 -0500

    moved vocalization to SpeechMaker

--- AppTextToSpeech/Program.cs
+++ AppTextToSpeech/Program.cs
@@ -2,47 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Speech.Synthesis;
 
 namespace AppTextToSpeech
 {
   enum AudioSource { AudioStream, DefaultAudioDevice, Null, WaveFile, WaveStream };
     
   class Program
-  {
-    static void Speech(string speechText, AudioSource outputSelection)
-    {
-      SpeechSynthesizer synth = new SpeechSynthesizer();
-      AudioSource source = outputSelection;
-      switch (source)
-      {
-        //case AudioSource.AudioStream:
-        //  synth.SetOutputToAudioStream();
-        //  break;
-        case AudioSource.Null:
-          synth.SetOutputToNull();
-          break;
-        //case AudioSource.WaveFile:
-        //  synth.SetOutputToWaveFile();
-        //  break;
-        //case AudioSource.WaveStream:
-        //  synth.SetOutputToWaveStream();
-        //  break;
-        case AudioSource.DefaultAudioDevice:
-        default:
-          synth.SetOutputToDefaultAudioDevice();
-          break;
-      }
-      synth.Speak(speechText);
-    } 
+  { 
     static void Main(string[] args)
     {
-      string speechText = "";
       var client = new MycroftClient("localhost", 1847);
       client.ListenForCommands();
-      AudioSource audioSource = AudioSource.DefaultAudioDevice;
-      //TODO: MycroftClient defines text and source
-      Speech(speechText, audioSource);
     }
   }
 }
