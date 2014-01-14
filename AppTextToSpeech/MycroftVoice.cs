d2837c5e27deb9f24e0873f7b33573bed4662487
Robert McLaughlin <robert@sparkk.us>
2014-01-14 11:09:59 -0500

    indentation fix

--- AppTextToSpeech/MycroftVoice.cs
+++ AppTextToSpeech/MycroftVoice.cs
@@ -7,30 +7,30 @@ using System.Speech.Synthesis;
 
 namespace AppTextToSpeech
 {
-    class MycroftVoice
-    {
-        private SpeechSynthesizer synth;
-        public MycroftVoice(){
-            InitializeVoice();
-        }
-
-        private void InitializeVoice()
-        {
-            synth = new SpeechSynthesizer();
-            synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0,
-                new System.Globalization.CultureInfo("en-GB"));
-        }
-
-        public void SayMessage(string message)
-        {
-            synth.Speak(message);
-        }
-
-        //Example Filepath: @"C:\Users\Joe\Desktop\Test.wav"
-        public void SaveMessage(string filepath, string message)
-        {
-            synth.SetOutputToWaveFile(filepath);
-            synth.Speak(message);
-        }
+  class MycroftVoice
+  {
+    private SpeechSynthesizer synth;
+    public MycroftVoice(){
+      InitializeVoice();
     }
+
+    private void InitializeVoice()
+    {
+      synth = new SpeechSynthesizer();
+      synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult, 0,
+        new System.Globalization.CultureInfo("en-GB"));
+    }
+
+    public void SayMessage(string message)
+    {
+      synth.Speak(message);
+    }
+
+    //Example Filepath: @"C:\Users\Joe\Desktop\Test.wav"
+    public void SaveMessage(string filepath, string message)
+    {
+      synth.SetOutputToWaveFile(filepath);
+      synth.Speak(message);
+    }
+  }
 }
